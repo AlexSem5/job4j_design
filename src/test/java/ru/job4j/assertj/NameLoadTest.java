000292eec69852760861c868823aef318cfdb1db
Aleksandr Semeikin <sensem@yandex.ru>
2022-10-06 21:11:22 +0300

    Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void whenNamesArrayIsEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("is empty");
+    }
+
+    @Test
+    void whenNoSymbol() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("VasiliyIvanov"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("the symbol");
+    }
+
+    @Test
+    void whenNoKey() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("=VasiliyIvanov"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("key");
+    }
+
+    @Test
+    void whenNoValue() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("VasiliyIvanov="))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("value");
+    }
+
+    @Test
+    void whenOk() {
+        NameLoad nameLoad = new NameLoad();
+        nameLoad.parse("Vasiliy=Ivanov", "Ivan=Petrov");
+        assertThat(nameLoad.getMap()).containsAllEntriesOf(Map.of(
+                "Vasiliy", "Ivanov",
+                "Ivan", "Petrov")
+        );
+    }
+}
