77512f89e23d51a20b06e9b02a13ff4fde6e8c2a
Aleksandr Semeikin <sensem@yandex.ru>
2022-10-25 17:18:51 +0300

    0.1. FileOutputStream. [#252488] Фиксация изменений

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,23 +1,22 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class ResultFile {
-    public static int[][] multiple(int size) {
-        int[][] matrix = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = (i + 1) * (j + 1);
-            }
-        }
-        return matrix;
-    }
-
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write(Arrays.deepToString(multiple(5)).getBytes());
-        } catch (Exception e) {
+            int size = 5;
+            int[][] matrix = new int[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    matrix[i][j] = (i + 1) * (j + 1);
+                    out.write((matrix[i][j] + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
