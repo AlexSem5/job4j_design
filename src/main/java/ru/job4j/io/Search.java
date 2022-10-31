4d45925ec7e2c4e560db0bb85c809fc08fe0e029
Aleksandr Semeikin <sensem@yandex.ru>
2022-10-31 18:52:29 +0300

    5. Валидация параметров запуска. [#246865 #356335] Фиксация изменений

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,7 +10,7 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        new Search().validate(args);
+        validate(args);
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
@@ -20,14 +21,20 @@ public class Search {
         return searcher.getPaths();
     }
 
-    private boolean validate(String[] args) {
-        if (args[0].length() == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+    private static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Two arguments are required");
         }
-        if (!args[1].startsWith(".")) {
+        File file = Paths.get(args[0]).toFile();
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (args[1].length() <= 1 || !args[1].startsWith(".")) {
             throw new IllegalArgumentException(
                     String.format("incorrect file extension: %s ", args[1]));
         }
-        return true;
     }
 }
