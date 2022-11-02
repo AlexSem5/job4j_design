63d99e77e6e0ff364836ddde1141d9730ce1e093
Aleksandr Semeikin <sensem@yandex.ru>
2022-11-02 17:44:34 +0300

    5.2. Архивировать проект [#861] Фиксация изменений

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -12,7 +13,7 @@ public class Zip {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (Path path : sources) {
                 zip.putNextEntry(new ZipEntry(path.toString()));
-                try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(path.toString()))) {
+                try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(path.toFile()))) {
                     zip.write(in.readAllBytes());
                 }
             }
@@ -21,19 +22,32 @@ public class Zip {
         }
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static void validate(ArgsName argsName) {
+        Path path = Paths.get(argsName.get("d"));
+        if (!Files.exists(path)) {
+            throw new IllegalArgumentException(String.format("Does not exist %s", path.toAbsolutePath()));
+        }
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException(String.format("Not a directory %s", path.toAbsolutePath()));
+        }
+        String extension = argsName.get("e");
+        if (!extension.matches("^[.][0-9a-z_]+$")) {
+            throw new IllegalArgumentException(
+                    String.format("incorrect file extension: %s ", extension));
+        }
+        String target = argsName.get("o");
+        if (!target.matches(".+[.].+")) {
+            throw new IllegalArgumentException(
+                    String.format("incorrect target name: %s ", target));
         }
     }
 
     public static void main(String[] args) throws IOException {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Three arguments are required");
+        }
         ArgsName zip = ArgsName.of(args);
+        validate(zip);
         Path start = Paths.get(zip.get("d"));
         List<Path> list = Search.search(start, p -> !p.toFile().getName().endsWith(zip.get("e")));
         Zip zip1 = new Zip();
