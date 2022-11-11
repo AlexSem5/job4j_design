1fc9239a34812b304301c33ced2358dc020a3989
Aleksandr Semeikin <sensem@yandex.ru>
2022-11-11 17:14:07 +0300

    Update

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -10,33 +10,36 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Клиент отправляет запросы, сервер должен ему отвечать
+ */
 public class EchoServer {
-	private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
-	
-	public static void main(String[] args) {
-		try (ServerSocket server = new ServerSocket(9000)) {
-			while (!server.isClosed()) {
-				Socket socket = server.accept();
-				try (OutputStream out = socket.getOutputStream();
-					 BufferedReader in = new BufferedReader(
-							 new InputStreamReader(socket.getInputStream()))) {
-					out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-					String line = in.readLine();
-					if (line != null && !line.isEmpty() && line.matches("^.+[?]msg=Exit.+$")) {
-						server.close();
-					}
-					if (line != null && !line.isEmpty() && line.matches("^.+[?]msg=Hello.+$")) {
-						out.write("Hello".getBytes());
-					}
-					if (line != null && !line.isEmpty()
-						&& !line.matches("^.+[?]msg=Exit.+$") && !line.matches("^.+[?]msg=Hello.+$")) {
-						out.write("What".getBytes());
-					}
-					out.flush();
-				}
-			}
-		} catch (IOException e) {
-			LOG.error("job4j Exception", e);
-		}
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+    
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String line = in.readLine();
+                    if (line != null && !line.isEmpty() && line.matches("^.+[?]msg=Exit.+$")) {
+                        server.close();
+                    }
+                    if (line != null && !line.isEmpty() && line.matches("^.+[?]msg=Hello.+$")) {
+                        out.write("Hello".getBytes());
+                    }
+                    if (line != null && !line.isEmpty()
+                        && !line.matches("^.+[?]msg=Exit.+$") && !line.matches("^.+[?]msg=Hello.+$")) {
+                        out.write("What".getBytes());
+                    }
+                    out.flush();
+                }
+            }
+        } catch (IOException e) {
+            LOG.error("job4j Exception", e);
+        }
+    }
 }
